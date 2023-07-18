831998a29b3ca7721f229b1e38492929fd625088
Austin Jenkins <austinclarkjenkins@gmail.com>
2023-07-18 17:18:19 -0600

    Revert back to monolith with react and ssr

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,47 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 use App\Models\Answer;
 use App\Models\Question;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Validator;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return 'As of now, this application is only an API. Please use the API endpoints to interact with the application. You can <a href="/create-question">Add a question</a> here.';
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+// Route::get('/', function () {
+//     return 'As of now, this application is only an API. Please use the API endpoints to interact with the application. You can <a href="/create-question">Add a question</a> here.';
+// });
 
 Route::get('/create-question', function () {
     return view('create-questions', [
