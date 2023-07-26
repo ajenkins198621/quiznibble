35cec87b3fbc9dff6ee74ec142b1aa3fd7c822b2
Austin Jenkins <austinclarkjenkins@gmail.com>
2023-07-26 09:03:40 -0600

    add scores to database

---
+++ database/migrations/2023_07_26_150137_add_scores_to_user_streaks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('user_streaks', function (Blueprint $table) {
+            $table->integer('day_score')->default(0)->after('last_quiz_date');
+            $table->integer('week_score')->default(0)->after('day_score');
+            $table->integer('total_score')->default(0)->after('week_score');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user_streaks', function (Blueprint $table) {
+            $table->dropColumn('day_score');
+            $table->dropColumn('week_score');
+            $table->dropColumn('total_score');
+        });
+    }
+};
